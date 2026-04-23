625e9f7774f91de10d5ec59277f3b71533de8c38
Enzo <bib.enzoarchambaud@gmail.com>
2026-04-23 10:51:43 +0200

    fetch sinistres

--- assurmobile/assurmobile/app/index.tsx
+++ assurmobile/assurmobile/app/index.tsx
@@ -1,14 +1,26 @@
-import { Pressable, Text, View } from "react-native";
-import { useState } from "react";
-import { useRootNavigationState, useRouter } from "expo-router";
-import { Button } from "react-native-paper";
+import { StyleSheet, ScrollView } from "react-native";
+import { useEffect, useState } from "react";
+import { Redirect, useRootNavigationState, useRouter } from "expo-router";
+import { Button, Crad, Text } from "react-native-paper";
 import { useCurrentUser } from "@/contexts/UserContext";
 
 export default function Index() {
-  const [value, onChangeTitle] = useState('test')
   const router = useRouter();
+  const [ sinistres, setSinistres ] = useState([]);
   const rootNavigationState = useRootNavigationState();
   const { user } = useCurrentUser();
+
+  useEffect(() => {
+    fetch('https://palace-ramble-champion.ngrok-free.dev/sinistres').then(async response => {
+      const data = await response.json();
+      if(response.status === 200) {
+        setSinistres(data)
+      }
+      console.log('Error', data)
+    }).catch(error => {
+      console.log('Error', error.message)
+    })
+  }, [])
 
   if (!user) {
     return <Redirect href="/login"/>
@@ -16,27 +28,28 @@ export default function Index() {
 
   if(rootNavigationState?.key) {
     return (
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Text>To Edit this value : {value}.</Text>
-        <Pressable
-          onPress={() => {
-            onChangeTitle('new Value')
-          }}
+      <ScrollView>
+        <Card
+          style={styles.card}
         >
-          <Text>Press on this link</Text>
-        </Pressable>
-        <Button
-          onPress={() => router.navigate('/login')}
-        >
-          Se connecter
-        </Button>
-      </View>
+          <Card.Title title="Sinistre 1" subtitle="Accident impliquant un tiers"/>
+          <Card.Content>
+            <Text variant="titleLarge">Véhicule : AA-123-BB</Text>
+            <Text variant="bodyMedium">Soumis le : 01/01/2026</Text>
+          </Card.Content>
+          <Card.Action>
+            <Button>Accéder au sinistre</Button>
+          </Card.Action>
+        </Card>
+      </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  card: {
+    marginTop: 12;
+    marginLeft: 10,
+    marginRight: 10,
+  }
+})
